fa0951ab61fbe9cf3b76d7d689b6a1fa37100002
ms3221 <hyeongjun3221@gmail.com>
2022-01-05 22:58:59 +0900

    test

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI:'mongodb+srv://kimgwhjg:rla121457@gunk.ouvru.mongodb.net/Watto?retryWrites=true&w=majority'
-}
+    mongoURI:'mongodb+srv://jun:1234@cluster0.zjit4.mongodb.net/WATTO?retryWrites=true&w=majority'
+}            
