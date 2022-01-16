fe7bdfec6435eeecc4352d1938be566cff950c89
ssongGD <zxas645@naver.com>
2022-01-16 22:06:35 +0900

    merge 0116-3

--- contract/contracts/NftWT.sol
+++ contract/contracts/NftWT.sol
@@ -2,13 +2,13 @@
     // SPDX-License-Identifier: MIT
     pragma solidity ^0.8.0;
 
-    import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; 
-    import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-    import "@openzeppelin/contracts/utils/Counters.sol";
-    import "@openzeppelin/contracts/utils/Counters.sol";
-    import "@openzeppelin/contracts/access/Ownable.sol";
-    import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-    import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+    import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol"; 
+    import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+    import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+    import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+    import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+    import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+    import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
     contract WATTONFT is ERC721URIStorage, Ownable {
