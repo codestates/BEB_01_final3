c7e38ec4257b915dbd2abcf2380e78abe3b3308a
kgd7054 <kgd7054@gmail.com>
2022-01-19 01:47:00 +0900

    교환소 마무리

--- contract/contracts/NWTToken.sol
+++ contract/contracts/NWTToken.sol
@@ -5,21 +5,28 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract NWTToken is ERC20, Ownable {
+    IERC20 public token1;  // stable coin
+    address public owner1;  // user. (token1의 소유자 계정 주소)
+    IERC20 public token2;  // 유동성 코임
+    address public owner2; // server address
+
     constructor() ERC20("NWToken", "NWT") {
-        
+        // token1 = IERC20(_token1);
+        // owner2 = serverAddress;
     }
     
-    function mintToken(address to, uint256 amount, address contractAddress) public onlyOwner returns (bool) {  // NFT 구매할 FT 토큰
+    function mintToken(address to, uint256 amount, address contractAddress1, address contractAddress2) public onlyOwner returns (bool) {  // NFT 구매할 FT 토큰
         require(to != address(0x0));
         require(amount > 0);
         _mint(to, amount);
         // _approve(to, msg.sender, allowance(to, msg.sender)+amount);  // approve 추가
-        _approve(to, contractAddress, allowance(to, contractAddress) + amount);  // approve 추가
+        // _approve(to, contractAddress1, allowance(to, contractAddress1) + amount);  // approve 추가 => nft 컨트랙트 연결
+        // _approve(to, contractAddress2, allowance(to, contractAddress2) + amount);  // approve 추가 => swap 컨트랙트 연결
 
         return true;
     }
 
-    function approveToken(address owner,address spender)public {
+    function approveToken(address owner,address spender) public {
          uint256 amount = balanceOf(owner);
         _approve(owner,spender,amount);
     }
