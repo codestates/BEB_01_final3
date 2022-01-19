9e3dac728964d9820de05bb2ce25f97199733091
kgd7054 <kgd7054@gmail.com>
2022-01-19 18:22:39 +0900

    관리자페이지 token 완료

---
+++ client/src/components/DeveloperPage/Spinner.js
@@ -0,0 +1,15 @@
+import ClipLoader from 'react-spinners/ClipLoader';
+
+const Spinner = () => {
+	return (
+		<ClipLoader
+			size='100'
+			height='160'
+			width='32'
+			color='#6b5ce7'
+			radius='8'
+		/>
+	);
+};
+
+export default Spinner;
