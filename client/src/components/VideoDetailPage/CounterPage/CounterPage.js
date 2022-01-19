ad94fb0d2ebda42bd16da737c5bec024d2288282
kimgwhjg <gwkim@pusan.ac.kr>
2022-01-19 15:45:30 +0900

    merge, 카운터페이지 데이터

--- client/src/components/VideoDetailPage/CounterPage/CounterPage.js
+++ client/src/components/VideoDetailPage/CounterPage/CounterPage.js
@@ -3,101 +3,111 @@ import axios from 'axios';
 import moment from 'moment'
 import { useCallback } from 'react';
 import { useParams } from "react-router-dom";
+import { Form, Row, Col} from 'react-bootstrap';
 
 
 function CounterPage() {
+  const videoId = useParams().videoId;
+  const variable = { videoId: videoId };
+  const [VideoDetail, setVideoDetail] = useState([]);
+  const [Survival, setSurvival] = useState("");
 
-    const videoId = useParams().videoId;
-    const variable = { videoId: videoId };
-    const [VideoDetail, setVideoDetail] = useState([]);
-    const [Survival, setSurvival] = useState('');
+  useEffect(async () => {
+    await axios.post("/api/video/getVideoDetail", variable).then((response) => {
+      if (response.data.success) {
+        console.log("getvideodata", response.data);
+        setVideoDetail(response.data.videoDetail);
+        const Member = response.data.videoDetail.survival[0].split(", ");
+        setSurvival(Member);
+      } else {
+        alert("비디오 정보를 가져오길 실패했습니다.");
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.post('/api/video/getVideoDetail', variable).then((response) => {
-            if (response.data.success) {
-                console.log('getvideodata', response.data);
-                setVideoDetail(response.data.videoDetail);
-                setSurvival(response.data.videoDetail.survival);
-            } else {
-                alert('비디오 정보를 가져오길 실패했습니다.');
-            }
-        });
-    }, []);
+  console.log("s1", Survival[0]);
+  console.log("s", Survival);
+  //랜더링 되자 마자 videoData 를 제일 먼저 호출해버림 = undefined
+  //console.log('videoData', VideoDetail.survival) => 아직 useEffect가 호출되지 않은상태이고
+  //따라서 그 값을 불러오지 못함
+  //그래서 useEffect 안에서 함수처리를 다해주고
+  //함수를 useState로 감싸서 들고 나온다.
 
-    // console.log('date', VideoDetail.opendate)
-    console.log('videoData', Survival[0])
-    
-    const people = Survival[0];
-    console.log(people)
-    //랜더링 되자 마자 videoData 를 제일 먼저 호출해버림 = undefined 
-    //console.log('videoData', VideoDetail.survival) => 아직 useEffect가 호출되지 않은상태이고
-    //따라서 그 값을 불러오지 못함
-    //그래서 useEffect 안에 response.data.videoDetail.survival 을 useState 값으로 지정하고
-    // console.log('videoData', Survival[0])  : Survival => useState에 선언이 되어있는 상태고 빈 상태이기 때문에
-    // useEffect가 다시한번 돌아가면서 채워줌 
+  const countDownTimer = useCallback((date) => {
+    let _vDate = moment(date);
+    let _second = 1000;
+    let _minute = _second * 60;
+    let _hour = _minute * 60;
+    let _day = _hour * 24;
+    let timer;
 
-    //useEffect 함수 호출, 
-    //getvideodata 로그
-    //videoData 로그
-    //getvidodata 로그
-    //videoData 로그 
-    
+    function showRemaining() {
+      try {
+        let now = moment();
+        let distDt = _vDate - now;
 
-    // const people = VideoDetail.survival[0];
-    // const z = useMemo(() => people, VideoDetail.survival[0])
-    // console.log('pe', people);
-    // const arr1 = people.split(", ");
-    // console.log('arr', arr1);
-    // console.log('arr1', arr1[0]);
-
-    // const timestamps = VideoDetail.opendate
-    // const opendate = new Date(timestamps)
-    // console.log(opendate)
-    //Number
-
-    // const hoursMinSecs = {hours:10, minutes: 20, seconds: 40}
-    const countDownTimer = useCallback((date) => {
-        let _vDate = moment(date);
-        let _second = 1000;
-        let _minute = _second * 60;
-        let _hour = _minute * 60;
-        let _day = _hour * 24;
-        let timer;
-
-        function showRemaining() {
-            try {
-                let now = moment();
-                let distDt = _vDate - now;
-
-                if (distDt < 0) {
-                    clearInterval(timer);
-                    let HapDate = '0' + 'd ' + '0' + 'h ' + '0' + 'm ' + '0' + 's';
-                    document.getElementById('timer').innerHTML = HapDate;
-                    return;
-                }
-                let days = Math.floor(distDt / _day);
-                let hours = Math.floor((distDt % _day) / _hour);
-                let minutes = Math.floor((distDt % _hour) / _minute);
-                let seconds = Math.floor((distDt % _minute) / _second);
-
-                let HapDate = parseInt(days) + 'd ' + parseInt(hours) + 'h ' + parseInt(minutes) + 'm ' + parseInt(seconds) + 's';
-                document.getElementById('timer').innerHTML = HapDate;
-            }
-            catch (e) {
-                console.log(e);
-            }
+        if (distDt < 0) {
+          clearInterval(timer);
+          let HapDate = "0" + "d " + "0" + "h " + "0" + "m " + "0" + "s";
+          document.getElementById("timer").innerHTML = HapDate;
+          return;
         }
-        timer = setInterval(showRemaining, 1000);
-    }, []);
+        let days = Math.floor(distDt / _day);
+        let hours = Math.floor((distDt % _day) / _hour);
+        let minutes = Math.floor((distDt % _hour) / _minute);
+        let seconds = Math.floor((distDt % _minute) / _second);
 
-    return (
-        <div>
-            <span id="timer">
-                개시 일
-            {countDownTimer(VideoDetail.opendate)}
-            </span>
-        </div>
-    )
+        let HapDate =
+          parseInt(days) +
+          "d " +
+          parseInt(hours) +
+          "h " +
+          parseInt(minutes) +
+          "m " +
+          parseInt(seconds) +
+          "s";
+        document.getElementById("timer").innerHTML = HapDate;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    timer = setInterval(showRemaining, 1000);
+  }, []);
+
+  return (
+    <div>
+      <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            Radios
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check
+              type="radio"
+              label="first radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="radio"
+              label="second radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="radio"
+              label="third radio"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+          </Col>
+        </Form.Group>
+      </fieldset>
+      <div>
+        <span id="timer">{countDownTimer(VideoDetail.opendate)}</span>
+      </div>
+    </div>
+  );
 }
 
-export default CounterPage
+export default CounterPage;
