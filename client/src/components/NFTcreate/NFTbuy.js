b6b12770906e7a90232298eadca2b3447f7f6d25
ssongGD <zxas645@naver.com>
2022-01-21 22:35:07 +0900

    merge 0121

--- client/src/components/NFTcreate/NFTbuy.js
+++ client/src/components/NFTcreate/NFTbuy.js
@@ -50,21 +50,30 @@ function NFTbuy ({nftlist}) {
     {
              nftlist.map((el)=>{
                return(
-                <Card style={{ width: '19rem', margin:"1.5%", cursor:"pointer"}} bg='black' text='white'>
+                <Card style={{ width: '19rem', margin:"1.5%", cursor:"pointer"}} bg='black' text='white' border='white'>
                 <Card.Img variant="top" src={el.imgUri} style={{height:'100%', width:'100%', }} />
                 <Card.Body style={{marginBottom: '0px', borderBottom: '1px solid #DCDCDC'}}>
-                  <Card.Title style={{textAlign:'left', marginTop: '3%', marginLeft:'-5%'}}>Content : {el.contentTitle}</Card.Title>
-                  <Card.Title style={{textAlign:'left', marginTop: '5%', marginLeft:'-5%'}}>Name : {el.nftName}</Card.Title>
-                  <Card.Title style={{textAlign:'left', marginTop: '5%', marginLeft:'-5%'}}>Price : {el.price}</Card.Title>
+                  <Card.Title style={{textAlign:'left', marginTop: '3%', marginLeft:'-3%'}}>
+                    Content : {el.contentTitle}
+                  </Card.Title>
+                  <Card.Title style={{textAlign:'left', marginTop: '5%', marginLeft:'-3%'}}>
+                    Name : {el.nftName}
+                  </Card.Title>
+                  <Card.Title style={{textAlign:'left', marginTop: '5%', marginLeft:'-3%'}}>
+                    Price : {el.price}
+                  </Card.Title>
                 </Card.Body>
                 <Card.Body style={{marginBottom: '0px', borderBottom: '1px solid #DCDCDC'}}>
-                  <Card.Text style={{textAlign:'left', marginLeft:'-5%', fontSize:'20px'}}>
+                  <Card.Text style={{textAlign:'left', marginLeft:'-3%', fontSize:'20px'}}>
                     desription : {el.description}
                   </Card.Text>
                 </Card.Body>
-                <Card.Body>
-                  <Button variant="warning" style={{fontWeight:"bold"}}  onClick={()=>{BuyNFT(el.tokenId)}} >판매중</Button>
-                  <LikeDisLike userId={localStorage.getItem('userId')} nftId={ el._id }/>
+                <Card.Body style={{display:"flex", marginLeft:'-3%', marginRight:'-9%'}}>
+                  <div>
+                    <Button variant="warning" style={{fontWeight:"bold"}}  onClick={()=>{BuyNFT(el.tokenId)}} >판매중</Button>
+                  </div>
+                  <div style={{width:"60%"}}></div>
+                  <LikeDisLike userId={localStorage.getItem('userId')} nftId={ el._id } />
                 </Card.Body>
                   
                 
