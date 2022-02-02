72fb4d5554aa7f5edee8424410ccd6e10f366e33
ms3221 <hyeongjun3221@gmail.com>
2022-02-02 23:41:13 +0900

    auctionlist spinner change

--- server/controller/nft.js
+++ server/controller/nft.js
@@ -436,11 +436,13 @@ module.exports = {
 		const beforePrice = req.body.beforePrice;
 	
 	
-	    
+		console.log(Bowner);
+		console.log(tokenId);
+		
 
 		if (spender !== Bowner) {
 			console.log('auction 함수에 이상이 생겼습니다.');
-			return res.status(500).json({ success: false });
+			return res.json({ success: false });
 		} else if (bidPrice <= beforePrice) {
 			console.log('제시금액이 너무 적다. 다시 측정해주라!');
 			return res.json({ success: false, detail:" Your Price is to Low" });
