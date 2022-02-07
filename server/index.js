9b3a038abada19ce836ebde309c99f336c0f352f
kimgwhjg <gwkim@pusan.ac.kr>
2022-02-08 00:31:25 +0900

    mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/prod');
@@ -19,7 +20,7 @@ const { myPage } = require('./controller/api');
 const mongoose = require('mongoose');
 
 mongoose
-	.connect(config.mongoURI)
+	.connect(config.mongoURI,{ useNewUrlParser: true })
 	.then(() => console.log('MongoDB Connected success !!'))
 	.catch((err) => console.log(err));
 
